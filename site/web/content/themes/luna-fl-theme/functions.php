eb80630a5f98eb4c7c85df54663734a547ac101b
Pallacci <a.pallacci@lunaflpartner.it>
2019-11-13 18:11:05 +0100

    ho creato un form e ho aggiunto un file css

--- site/web/content/themes/luna-fl-theme/functions.php
+++ site/web/content/themes/luna-fl-theme/functions.php
@@ -3,5 +3,13 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+
+	wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+   wp_enqueue_style( 'stiloso', get_stylesheet_directory_uri().'/stiloso.css' );
+   
 }
 ?>
